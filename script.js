88170e7b4a452b9539c0165272d7bb3ce7c3701b
mostone1 <129112059+mostone1@users.noreply.github.com>
2023-05-18 19:32:39 +0000

    homework1

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+//task 1
 
+let a = 10;
+let b = 20;
+let c = 30;
+
+console.log(a < b < c);
+
+//task 2
+
+let x = 1;
+let y = 2;
+
+let res1 = String(x) + String(y);
+console.log(res1);
+console.log(typeof res1);
+
+let res2 = Boolean(x) + y;
+console.log(res2);
+console.log(typeof res2);
+
+let res3 = x < y;
+console.log(res3);
+console.log(typeof res3);
+
+let res4 = (x / 0) / (y / 0);
+console.log(res4);
+console.log(typeof res4);
+
+//task 3
+
+function age() {
+    let isAdult = prompt("How old are you?");
+    if (isAdult >= 18 ) {
+        alert("Ви досягли повнолітнього віку");
+    } else {
+        alert("Ви ще надто молоді");
+    }   
+}
+
+age()
+
+//task 4
+
+let arr = [4, 5, 2, 1, 6, 5, 3, 5, 2, 5]
+
+
+//task 5
